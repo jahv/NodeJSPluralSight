f7a403b5021589c321676491dd24601b4ee1c011
Jose Antonio Hernandez <ahernandez@nearsoft.com>
2017-08-14 22:01:49 -0500

    Express

--- gradeBook/program.js
+++ gradeBook/program.js
@@ -1,8 +1,36 @@
 var gradeBook = require("./lib/gradeBook").gradeBook;
 
-for(var i=2; i<process.argv.length; i++) {
-  gradeBook.addGrade(parseInt(process.argv[i]));
-}
+/**
+ * Before express
+ */
+// for(var i=2; i<process.argv.length; i++) {
+//   gradeBook.addGrade(parseInt(process.argv[i]));
+// }
+// console.log(gradeBook.getAverage());
+// console.log(gradeBook.getLetterGrade());
 
-console.log(gradeBook.getAverage());
-console.log(gradeBook.getLetterGrade());
+/**
+ * With express
+ */
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+  res.send("Hello, World!");
+});
+
+/**
+ * /grade?grades=90,80,70
+ */
+app.get("/grade", function(req, res){
+  var grades = req.query.grades.split(",");
+  for(var i=0; i<grades.length; i++) {
+    gradeBook.addGrade(parseInt(grades[i]));
+  }
+  var average = gradeBook.getAverage();
+  var gradeLetter = gradeBook.getLetterGrade();
+  res.send("Your average is " + average + ", grades is " + gradeLetter);
+});
+
+app.listen(3000);
+console.log("Server ready...");
